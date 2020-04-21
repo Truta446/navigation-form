7eebf8b1b88fca1c37da272bc715f5d307ac7ec8
Juan V Lopes <juan.versolato@outlook.com>
2020-04-21 10:59:42 -0400

    Config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,9 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-plusplus': 'off',
+    'prefer-destructuring': ['error', {
+      'array': false,
+      'object': true,
+    }],
   },
 };
