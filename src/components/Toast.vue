b8cd82b3075d32eb2911f9d2b25fa974c482c024
Juan V Lopes <juan.versolato@outlook.com>
2020-04-22 17:02:28 -0400

    New component

---
+++ src/components/Toast.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-snackbar
+      v-model="snackbar"
+      :color="color"
+      :multi-line="true"
+      :right="true"
+      :timeout="timeout"
+      :top="true"
+    >
+      {{ text }}
+      <v-btn
+        icon
+        v-on:click="snackbar = false"
+      >
+        <v-icon color="white">mdi-close-box-outline</v-icon>
+      </v-btn>
+    </v-snackbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['color', 'text'],
+  data: () => ({
+    snackbar: true,
+    timeout: 3000,
+  }),
+};
+</script>
